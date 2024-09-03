7128e301451fb8782560ef0c82028f43ed45924d
Neema <patelneema93@gmail.com>
2024-09-03 15:28:34 -0400

    create dummy data and fetch th data

--- first-app/src/app/user/user.component.ts
+++ first-app/src/app/user/user.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { DUMMY_USERS } from '../dammy-user';
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+selecteduser = DUMMY_USERS[randomIndex]
 }
