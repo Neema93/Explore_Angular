224eddbf148951ca50b8664db984a15cbcf7621b
Neema <patelneema93@gmail.com>
2024-09-04 09:48:24 -0400

    create user model

--- first-app/src/app/user/user.component.ts
+++ first-app/src/app/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dammy-user';
+import { type User } from './user.model';
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
@@ -8,10 +9,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  
-selecteduser = DUMMY_USERS[randomIndex];
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+// selecteduser = DUMMY_USERS[randomIndex];
 
 get imagePath() {
-  return '/asserts/users/' + this.selecteduser.avatar;
+  return '/asserts/users/' + this.user.avatar;
+}
+
+onSelectUser() {
+  this.select.emit(this.user.id);
 }
 }
