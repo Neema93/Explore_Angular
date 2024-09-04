67c72ef9e2a550882a02f0972d80f28f6c56d288
Neema <patelneema93@gmail.com>
2024-09-04 16:07:05 -0400

    add task component

---
+++ first-app/src/app/tasks/dammy-task.ts
@@ -0,0 +1,33 @@
+export const DUMMY_TASKS = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Complete Project Report',
+      summary: 'Finalize and submit the project report for the first quarter.',
+      dueDate: '2024-06-10',
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Prepare Presentation',
+      summary:
+        'Create a PowerPoint presentation for the upcoming client meeting.',
+      dueDate: '2024-06-15',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Team Meeting',
+      summary:
+        'Organize and lead the bi-weekly team meeting to discuss project progress.',
+      dueDate: '2024-06-05',
+    },
+    {
+      id: 't4',
+      userId: 'u3',
+      title: 'Team Meeting',
+      summary:
+        'Organize and lead the bi-weekly team meeting to discuss project progress.',
+      dueDate: '2024-06-05',
+    },
+  ];
--- first-app/src/app/tasks/tasks.component.ts
+++ first-app/src/app/tasks/tasks.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
-
+import { AddtaskComponent } from './addtask/addtask.component';
+import {DUMMY_TASKS} from './dammy-task';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [AddtaskComponent, TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  //@Input({ required: true }) userId!: string;
+  @Input({ required: true }) userId!: string;
   @Input({ required: true }) name!: string;
+ 
 }
