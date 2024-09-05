f6720f13d7ba7de2d54b8d97dddcbb65e1fc867d
Neema <patelneema93@gmail.com>
2024-09-05 15:02:59 -0400

    all method working with sevice

--- first-app/src/app/tasks/addtask/addtask.component.ts
+++ first-app/src/app/tasks/addtask/addtask.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
+import { TaskService } from '../tasks.service';
 
 @Component({
   selector: 'app-addtask',
@@ -10,21 +11,23 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './addtask.component.css'
 })
 export class AddtaskComponent {
-  @Output() cancel = new EventEmitter<void>();
+  @Input ({required:true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
   @Output() add = new EventEmitter<NewTaskData>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
-
+  private taskService = inject(TaskService)
   onCancle(){
-    this.cancel.emit();
+    this.close.emit();
   }
   onSubmit() {
-   this.add.emit({
+   this.taskService.addTask({
     title: this.enteredTitle,
     summary:this.enteredSummary,
     date:this.enteredDate
-   }) 
+   }, this.userId);
+   this.close.emit();
    
 }
 }
--- first-app/src/app/tasks/task/task.component.ts
+++ first-app/src/app/tasks/task/task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, inject, Input,} from '@angular/core';
 import { Task } from './task.model';
 import { DatePipe } from '@angular/common';
+import { TaskService } from '../tasks.service';
 @Component({
   selector: 'app-task',
   standalone: true,
@@ -11,8 +12,9 @@ import { DatePipe } from '@angular/common';
 export class TaskComponent {
 
   @Input({ required: true }) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+
+  private taskService = inject(TaskService)
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    this.taskService.removeTask(this.task.id);
   }
 }
