81ff716667ca75c63268cd6ba195bb230a886403
Neema <patelneema93@gmail.com>
2024-09-05 15:10:12 -0400

    data store in local storage

--- first-app/src/app/tasks/tasks.service.ts
+++ first-app/src/app/tasks/tasks.service.ts
@@ -4,6 +4,12 @@ import { NewTaskData } from "./task/task.model";
 @Injectable({providedIn: 'root'})
 export class TaskService {
     tasks= DUMMY_TASKS;
+    constructor(){
+        const tasks = localStorage.getItem('tasks');
+        if(tasks){
+            this.tasks = JSON.parse(tasks);
+        }
+    }
     getUserTasks(userId:string) {
         return this.tasks.filter((task) => task.userId === userId);
     }
@@ -16,8 +22,13 @@ export class TaskService {
             summary:taskData.summary,
             dueDate:taskData.date
           }) 
+          this.saveTasks();
     }
     removeTask(id: string) {
         this.tasks = this.tasks.filter((task)=> task.id !== id);
+        this.saveTasks();
+    }
+    private saveTasks() {
+        localStorage.setItem('tasks',JSON.stringify(this.tasks));
     }
 }
