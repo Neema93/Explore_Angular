b0d3e4bc28952ed5d9a51cc1af3063bc567c86b6
Neema <patelneema93@gmail.com>
2024-09-05 11:10:41 -0400

    add ngModel for twoway bunding

--- first-app/src/app/tasks/addtask/addtask.component.ts
+++ first-app/src/app/tasks/addtask/addtask.component.ts
@@ -1,14 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-addtask',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './addtask.component.html',
   styleUrl: './addtask.component.css'
 })
 export class AddtaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
+
   onCancle(){
     this.cancel.emit();
   }
