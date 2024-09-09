1f895a7f6db4c77ae8843d1b643fb0d4bbc3c8c9
Neema <patelneema93@gmail.com>
2024-09-09 09:46:01 -0400

    investment-result get data

--- investment-calculator/src/app/app.component.ts
+++ investment-calculator/src/app/app.component.ts
@@ -2,17 +2,25 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from "./user-input/user-input.component";
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserInputComponent],
+  imports: [RouterOutlet, HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'investment-calculator';
-annualData: string | undefined;
+  resultsData?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[];
   onCalculateInvestmentResults(data: {
     initialInvestment: number;
     duration: number;
@@ -40,6 +48,7 @@ annualData: string | undefined;
       });
     }
     console.log('Submitted!', annualData);
-    return annualData;
+    // return annualData;
+    this.resultsData = annualData;
   }
 }
