c98c01e25aa0ae0b0b2a3227c2dfcaf1e38f13ef
Neema <patelneema93@gmail.com>
2024-09-09 13:05:10 -0400

    Migrating to Module

--- investment-calculator/src/app/user-input/user-input.component.ts
+++ investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,21 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+
 import { InvestmentService } from '../investment.service';
 @Component({
   selector: 'app-user-input',
-  standalone: true,
-  imports: [FormsModule],
+
+
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
 
-  // @Output() calculate = new EventEmitter<{
-  //   initialInvestment: number;
-  //   duration: number;
-  //   expectedReturn: number;
-  //   annualInvestment: number;
-  // }>();
+ 
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal('0');
   enteredExpectedReturn = signal('5');
