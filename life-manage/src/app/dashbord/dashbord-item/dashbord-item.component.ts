77ecf24467469d8ce04bd42a96a7df8149c8923c
Neema <patelneema93@gmail.com>
2024-09-10 11:06:44 -0400

    create dashbord/dashbord-item component

---
+++ life-manage/src/app/dashbord/dashbord-item/dashbord-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashbord-item.component.html',
+  styleUrl: './dashbord-item.component.css'
+})
+export class DashbordItemComponent {
+
+}
