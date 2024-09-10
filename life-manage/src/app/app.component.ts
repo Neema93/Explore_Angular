c97e92d4a856210bc5fb5e05af06e81bc4cdb120
Neema <patelneema93@gmail.com>
2024-09-10 10:41:46 -0400

    sepatrate indecision test file

--- life-manage/src/app/app.component.ts
+++ life-manage/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { IndecisionComponent } from "./dashbord/indecision/indecision.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, IndecisionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
