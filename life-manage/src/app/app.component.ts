b243d1c4e5373dd8c602284066b45c44d0877336
Neema <patelneema93@gmail.com>
2024-09-10 10:44:50 -0400

    separate cooking decision test file

--- life-manage/src/app/app.component.ts
+++ life-manage/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { IndecisionComponent } from "./dashbord/indecision/indecision.component";
+import { CookingDecisionComponent } from "./dashbord/cooking-decision/cooking-decision.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, IndecisionComponent],
+  imports: [RouterOutlet, HeaderComponent, IndecisionComponent, CookingDecisionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
