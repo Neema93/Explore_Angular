9ddade1cbc504148ab983a3a2e92d94949d6c791
Neema <patelneema93@gmail.com>
2024-09-10 10:48:48 -0400

    sepate new ideas file

--- life-manage/src/app/app.component.ts
+++ life-manage/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { IndecisionComponent } from "./dashbord/indecision/indecision.component";
 import { CookingDecisionComponent } from "./dashbord/cooking-decision/cooking-decision.component";
+import { NewIdeasComponent } from "./dashbord/new-ideas/new-ideas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, IndecisionComponent, CookingDecisionComponent],
+  imports: [RouterOutlet, HeaderComponent, IndecisionComponent, CookingDecisionComponent, NewIdeasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
