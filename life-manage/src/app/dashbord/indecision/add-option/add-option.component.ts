c219a4d07ea3abfb17741f609b295ffa2c74a28b
Neema <patelneema93@gmail.com>
2024-09-10 15:22:47 -0400

    create add-option component

---
+++ life-manage/src/app/dashbord/indecision/add-option/add-option.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-option',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-option.component.html',
+  styleUrl: './add-option.component.css'
+})
+export class AddOptionComponent {
+
+}
