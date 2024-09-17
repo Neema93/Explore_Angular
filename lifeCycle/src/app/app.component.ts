56b4d9cc038df237fa32e0ae20d20839495cc802
Neema <patelneema93@gmail.com>
2024-09-17 09:30:36 -0400

    project working

--- lifeCycle/src/app/app.component.ts
+++ lifeCycle/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LifecycleComponent } from './lifecycle/lifecycle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LifecycleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lifeCycle';
+  onToggleLifecycle(){
+
+  }
+  onChangeLifecycle(){
+    
+  }
 }
