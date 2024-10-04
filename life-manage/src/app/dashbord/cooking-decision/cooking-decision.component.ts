377db8a685d611cd5e28f2ecacb79abfbeaf45df
Neema <patelneema93@gmail.com>
2024-10-04 10:43:42 -0400

    remove add-option

--- life-manage/src/app/dashbord/cooking-decision/cooking-decision.component.ts
+++ life-manage/src/app/dashbord/cooking-decision/cooking-decision.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AddOptionComponent } from "../cooking-decision/add-option/add-option.component";
 
 @Component({
   selector: 'app-cooking-decision',
   standalone: true,
-  imports: [],
+  imports: [AddOptionComponent],
   templateUrl: './cooking-decision.component.html',
   styleUrl: './cooking-decision.component.css'
 })
