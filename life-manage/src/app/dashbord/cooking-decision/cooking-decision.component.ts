f0446916c8f76c64d642fc5f81d85d8d8067821d
Neema <patelneema93@gmail.com>
2024-10-04 13:54:29 -0400

    indecision app error solve

--- life-manage/src/app/dashbord/cooking-decision/cooking-decision.component.ts
+++ life-manage/src/app/dashbord/cooking-decision/cooking-decision.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AddListComponent } from "./add-list/add-list.component";
+import { ListsComponent } from "./lists/lists.component";
 
 
 @Component({
   selector: 'app-cooking-decision',
   standalone: true,
-  imports: [AddListComponent],
+  imports: [AddListComponent, ListsComponent],
   templateUrl: './cooking-decision.component.html',
   styleUrl: './cooking-decision.component.css'
 })
--- life-manage/src/app/dashbord/indecision/indecision.component.ts
+++ life-manage/src/app/dashbord/indecision/indecision.component.ts
@@ -1,32 +1,42 @@
 import { Component } from '@angular/core';
-import { DashbordItemComponent } from "../dashbord-item/dashbord-item.component";
-import { AddOptionComponent } from "./add-option/add-option.component";
+import { DashbordItemComponent } from '../dashbord-item/dashbord-item.component';
+import { AddOptionComponent } from './add-option/add-option.component';
 import { OptionsService } from './option.service';
-import { OptionsComponent } from "./options/options.component";
+import { OptionsComponent } from './options/options.component';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-indecision',
   standalone: true,
-  imports: [DashbordItemComponent, AddOptionComponent, OptionsComponent, CommonModule,],
+  imports: [
+    DashbordItemComponent,
+    AddOptionComponent,
+    OptionsComponent,
+    CommonModule,
+  ],
   templateUrl: './indecision.component.html',
-  styleUrl: './indecision.component.css'
+  styleUrl: './indecision.component.css',
 })
 export class IndecisionComponent {
   isDisabled: boolean = false;
- 
-  constructor(private optionsService: OptionsService){}
- 
-  get selectOption(){
-    console.log(this.optionsService.getRandomOption())
-  
-    this.isDisabled =this.isDisabled ? false : true;
-    
-    return this.optionsService.getRandomOption().option
+  selectedOption: string | null = null;
+  constructor(private optionsService: OptionsService) {}
 
+  selectOption() {
+    if (!this.isDisabled) {
+      const randomOption = this.optionsService.getRandomOption();
+      if (randomOption) {
+        console.log(randomOption);
+
+        this.selectedOption = randomOption.option;
+        this.isDisabled = true; 
+      } else {
+        console.error('No options available'); 
+      }
+    }
   }
-  
+  resetSelection() {
+    this.isDisabled = false; 
+    this.selectedOption = null; 
+  }
 }
-
-
