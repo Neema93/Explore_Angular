c51de61a15156e66741cb00d96f21d221244e511
Neema <patelneema93@gmail.com>
2024-10-11 10:57:29 -0400

    create effect in constructor

--- rxjs/src/app/app.component.ts
+++ rxjs/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { interval, map, Subscription } from 'rxjs';
 
@@ -13,16 +13,22 @@ export class AppComponent implements OnInit {
   title = 'rxjs';
   clickCount = signal(0);
   private destroyRef = inject(DestroyRef);
-  ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(map((val) => val * 2))
-      .subscribe({
-        next: (val) => console.log(val),
-      });
 
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
+  constructor(){
+    effect(() => {
+      console.log(`click button ${this.clickCount} times`)
     });
+  }
+  ngOnInit(): void {
+    // const subscription = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
   }
   onClick() {
     this.clickCount.update(prevCount => prevCount + 1)
