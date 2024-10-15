ddd90446b00a9f7f1eaa00102879ed9ac3e46dd7
Neema <patelneema93@gmail.com>
2024-10-15 17:00:32 -0400

    write message in pipe

--- http/src/app/places/available-places/available-places.component.ts
+++ http/src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,9 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { Place } from '../place.model';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
-import { PlacesContainerComponent } from "../places-container/places-container.component";
-import { PlacesComponent } from "../places.component";
+import { catchError, map, throwError } from 'rxjs';
+import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { PlacesComponent } from '../places.component';
 @Component({
   selector: 'app-available-places',
   standalone: true,
@@ -15,8 +15,8 @@ export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
-isFetching = signal(false);
-error =signal('');
+  isFetching = signal(false);
+  error = signal('');
 
   // constructor(private httpClient: HttpClient) {}
 
@@ -25,18 +25,28 @@ error =signal('');
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
       .pipe(
-        map((resData) => resData.places)
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Plese try again later '
+              )
+          );
+  })
+
       )
       .subscribe({
         next: (places) => {
           this.places.set(places);
         },
-        error: (error) =>{
-          this.error.set(error.message)
+        error: (error: Error) => {
+          this.error.set(error.message);
         },
         complete: () => {
           this.isFetching.set(false);
-        }
+        },
       });
 
     this.destroyRef.onDestroy(() => {
