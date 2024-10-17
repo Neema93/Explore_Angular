d5b019c620cf6fe78cbd542fccfdba7a41ce777b
Neema <patelneema93@gmail.com>
2024-10-17 11:30:04 -0400

    Implementing optimistic update

--- http/src/app/places/places.service.ts
+++ http/src/app/places/places.service.ts
@@ -30,9 +30,10 @@ export class PlacesService {
       }));
   }
 
-  addPlaceToUserPlaces(placeId: string) {
+  addPlaceToUserPlaces(place: Place) {
+    this.userPlaces.update(prevPlace => [...prevPlace, place])
     return this.httpClient.put('http://localhost:3000/user-places', {
-      placeId,
+      placeId: place.id,
     });
   }
 
