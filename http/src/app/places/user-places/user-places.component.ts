e7cfc98075f3d9c513d5c81354d2bcc88dae1cb4
Neema <patelneema93@gmail.com>
2024-10-17 10:45:31 -0400

    fetch selected user

--- http/src/app/places/user-places/user-places.component.ts
+++ http/src/app/places/user-places/user-places.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { PlacesContainerComponent } from "../places-container/places-container.component";
-
+import { HttpClient } from '@angular/common/http';
+import { Place } from '../place.model';
+import { catchError, map, throwError } from 'rxjs';
+import { PlacesComponent } from "../places.component";
 @Component({
   selector: 'app-user-places',
   standalone: true,
-  imports: [PlacesContainerComponent],
+  imports: [PlacesContainerComponent, PlacesComponent],
   templateUrl: './user-places.component.html',
   styleUrl: './user-places.component.css'
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
 
+  places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  isFetching = signal(false);
+  error = signal('');
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(
+            () =>
+              new Error(
+                'Something went wrong fetching the available places. Plese try again later '
+              )
+          );
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
