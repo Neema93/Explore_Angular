d411efb72aa5a3ef788767bfa53e043002a49076
Neema <patelneema93@gmail.com>
2024-10-17 16:29:33 -0400

    set error in app component

--- http/src/app/app.component.ts
+++ http/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AvailablePlacesComponent } from "./places/available-places/available-places.component";
 import { UserPlacesComponent } from "./places/user-places/user-places.component";
+import { ErrorService } from './shared/error.service';
+import { ErrorModalComponent } from "./shared/modal/error-modal/error-modal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AvailablePlacesComponent, UserPlacesComponent],
+  imports: [RouterOutlet, AvailablePlacesComponent, UserPlacesComponent, ErrorModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private errorService = inject(ErrorService)
+  error = this.errorService.error;
   title = 'http';
 }
