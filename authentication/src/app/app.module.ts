1cc809eeb753c74dfe512ac880dd8bb2da479caa
Neema <patelneema93@gmail.com>
2024-10-22 14:23:47 -0400

    create auth form

--- authentication/src/app/app.module.ts
+++ authentication/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { AuthComponent } from "./auth/auth.component";
 
 @NgModule({
     declarations: [AppComponent,
-               
+               AuthComponent
                ],
     bootstrap:[AppComponent],
     imports:[BrowserModule]
