278ea3d92ff595b642f7a4d809f00da881cb02b7
Neema <patelneema93@gmail.com>
2024-10-30 16:13:07 -0400

    create lifecycle method

--- lifeCycle/src/app/lifecycle/lifecycle.component.ts
+++ lifeCycle/src/app/lifecycle/lifecycle.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './lifecycle.component.html',
   styleUrl: './lifecycle.component.css'
 })
-export class LifecycleComponent {
-  
+export class LifecycleComponent implements OnInit, OnChanges, DoCheck, AfterViewInit, AfterContentInit, OnDestroy {
+  value = 0;
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called', changes);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
+
+  changeValue(): void {
+    this.value++;
+  }
 }
